90a2e998b4e76292adda1699f13257a42b7264a3
Laurenz Seipel <laurenz.seipel@gmail.com>
2022-12-04 21:10:48 +0100

    setup nextauth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,90 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { loginSchema } from "../../../utils/authValidation";
+import { prisma } from "../../../server/db/client";
+import { env } from "process";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+const authOptions: NextAuthOptions = {
+  callbacks: {
+    async jwt({ token, user }) {
+      // Persist the OAuth access_token and or the user id to the token right after signin
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+        token.name = user.name;
+        token.email = user.email;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
+        session.user.name = token.name as string;
+        session.user.email = token.email as string;
+      }
+
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials, req) => {
+        const cred = await loginSchema.parseAsync(credentials);
+        // perform you login logic
+        // find out user from db
+        const user = await prisma.user.findFirst({
+          where: {
+            email: cred.email,
+          },
+        });
+        if (!user) {
+          return null;
+        }
+
+        // if everything is fine
+        const isValidPassword = bcrypt.compareSync(
+          cred.password,
+          user.password
+        );
+
+        if (!isValidPassword) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+  secret: env.NEXTAUTH_SECRET,
+  jwt: {
+    secret: env.NEXTAUTH_SECRET,
+  },
+  pages: {
+    signIn: "/login",
+    newUser: "/register",
+    error: "/login",
+  },
+};
+
+export default NextAuth(authOptions);
